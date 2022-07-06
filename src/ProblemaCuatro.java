6aea99f2a369669099e743ce2cb405a0cddd9ae8
Your Nam <alfonso.vargas@mercadolibre.com.co>
2022-07-06 12:16:22 -0500

    logica problema cuatro

--- src/ProblemaCuatro.java
+++ src/ProblemaCuatro.java
@@ -1,2 +1,20 @@
+import java.util.*;
+
 public class ProblemaCuatro {
+    public static void main(String[] args) {
+        System.out.println("cuantas personas?");
+        Scanner INPUT = new Scanner(System.in);
+        Integer persons = INPUT.nextInt();
+        if (persons <= 100){
+            List<Integer> numberOfHits = new ArrayList<>();
+            while (numberOfHits.size() < persons) {
+                numberOfHits.add((int)(Math.random() * 20 + 0));
+            }
+            int minNumber = Collections.min(numberOfHits);
+            int person = numberOfHits.indexOf(minNumber)+1;
+            System.out.println(person);
+        }else {
+            System.out.println("Numero no valido");
+        }
+    }
 }
